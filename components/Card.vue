e2075159d8899b77be02a3982cb461a2cd05a776
Your Name <ekka.beaula@gmail.com>
2024-08-03 20:43:37 +0200

    completed cards component

---
+++ components/Card.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  backgroundColor: { type: String, default: "bg-green-100" },
+});
+</script>
+
+<template>
+  <div :class="` ${backgroundColor} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped></style>
